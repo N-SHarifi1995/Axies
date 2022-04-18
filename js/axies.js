12268c4c9baf171ce8ae457dfd6c25408d77a5ee
noosheen1995 <noosheen.sh1995@yahoo.com>
2022-04-18 12:30:48 +0430

    seelaer part lis added

--- js/axies.js
+++ js/axies.js
@@ -2,15 +2,45 @@ let items = document.querySelectorAll(".item");
 
 items.forEach((ele) => {
     ele.addEventListener("mouseover", () => {
-        showremover()
+        showremover();
         ele.classList.add("show");
     });
-    ele.addEventListener("mouseleave", showremover)
-
+    ele.addEventListener("mouseleave", showremover);
 });
 
 function showremover() {
     items.forEach((el) => {
-        el.classList.remove('show')
-    })
+        el.classList.remove("show");
+    });
 }
+///////////////////////sliderpart////////////////
+let carts = document.querySelectorAll(".cart");
+console.log(carts)
+carts.forEach((cart) => {
+    cart.addEventListener("mouseover", () => {
+        clearInterval(player);
+    });
+    cart.addEventListener("mouseout", () => {
+        player = setInterval(play, 8);
+    });
+});
+let livpart = document.querySelector(".carts");
+
+let player = setInterval(play, 10);
+
+function play() {
+    let x = livpart.scrollWidth - livpart.clientWidth;
+    if (livpart.scrollLeft > x - 1) {
+        livpart.scrollLeft -= x;
+    } else {
+        livpart.scrollLeft += 1;
+    }
+}
+let leftbtn = document.querySelector(".arrow-left");
+let rightbtn = document.querySelector(".arrow-right");
+leftbtn.addEventListener('click', () => {
+    livpart.scrollLeft -= 50
+})
+rightbtn.addEventListener('click', () => {
+    livpart.scrollLeft += 50
+})
